a088ac85b67128a3c2c9c5823b9929adf0b604e6
Lisa Campbell <campb215@purdue.edu>
2020-10-08 18:44:36 -0500

    starting point

---
+++ DLDash/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+	  apiKey: "AIzaSyAzG6QsOXXGY1M1BpSecw9fPw8g5KAKpT8",
+	  authDomain: "dldash.firebaseapp.com",
+	  databaseURL: "https://dldash.firebaseio.com",
+	  projectId: "dldash",
+	  storageBucket: "dldash.appspot.com",
+	  messagingSenderId: "228798655856",
+	  appId: "1:228798655856:web:6010c280efcced1dc4f3b5",
+	  measurementId: "G-DZG3WT4QLZ"
+  }
+};
---
+++ DLDash/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+  	apiKey: "AIzaSyAzG6QsOXXGY1M1BpSecw9fPw8g5KAKpT8",
+    authDomain: "dldash.firebaseapp.com",
+    databaseURL: "https://dldash.firebaseio.com",
+    projectId: "dldash",
+    storageBucket: "dldash.appspot.com",
+    messagingSenderId: "228798655856",
+    appId: "1:228798655856:web:6010c280efcced1dc4f3b5",
+    measurementId: "G-DZG3WT4QLZ"
+  }
+};
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
